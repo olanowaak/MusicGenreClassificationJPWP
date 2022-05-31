353e64d3adbb511f14e1c8b56ccb630e9a977bef
olanowaak <56030627+olanowaak@users.noreply.github.com>
2022-05-31 20:06:57 +0200

    Update zadanie_1.py

--- zadanie_1.py
+++ zadanie_1.py
@@ -6,28 +6,29 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-x_train = np.random.rand(10, 3)  # generate 10 random vectors of dimension 3
-x_test = np.random.rand(3)  # generate one more random vector of the same dimension
-
+x_train = np.random.rand(10, 3)
+x_test = np.random.rand(3)
 
 def dist(a, b):
     sum = 0
     for ai, bi in zip(a, b):
-        sum = sum + (ai - bi) ** 2
-    return np.sqrt(sum)
-
+        # miejsce na twój kod
+        pass
+    return 0
 
 def nearest(x_train, x_test):
     nearest = -1
     min_distance = np.Inf
+
     # miejsce na twój kod
+
 
     print("twoja funkcja:")
     print(nearest)
-
 
 nearest(x_train, x_test)
 neigh = NearestNeighbors(n_neighbors=1)
 neigh.fit(x_train)
 print("\nsklearn.neighbors:")
 print(neigh.kneighbors([x_test], 1)[1][0][0])
+
